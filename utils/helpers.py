f329a9be2dab6d6782c1a52a6a69b309fb542ea4
realandrey <andrewbarulin@gmail.com>
2025-05-12 21:37:51 +0300

    add helpers.py

---
+++ utils/helpers.py
@@ -0,0 +1,15 @@
+import os
+from dotenv import load_dotenv
+
+load_dotenv()
+
+def get_env_variable(name):
+    value = os.getenv(name)
+    if not value:
+        raise ValueError(f"Environment variable '{name}' is not set")
+    return value
+
+
+CLICKUP_API_KEY = get_env_variable("CLICKUP_API_KEY")
+CLICKUP_EMAIL = get_env_variable("CLICKUP_EMAIL")
+CLICKUP_PASSWORD= get_env_variable("CLICKUP_PASSWORD")
