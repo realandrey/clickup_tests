45fd2072e99610f4f08b6b80ded0559491f89560
realandrey <andrewbarulin@gmail.com>
2025-05-13 19:48:24 +0300

    update api key

---
+++ constants.py
@@ -0,0 +1,11 @@
+BASE_URL = "https://api.pomidor-stage.ru"
+
+AUTH_HEADERS = {
+    "Content-Type": "application/json",
+    "Accept": "application/json"
+}
+
+API_HEADERS = {
+    "Content-Type": "application/json",
+    "Accept": "application/json"
+}
