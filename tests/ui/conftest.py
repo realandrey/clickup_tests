76891f803af3fe6f100e357d101e647391abcfc0
realandrey <andrewbarulin@gmail.com>
2025-05-19 20:41:58 +0300

    add allure steps

--- tests/ui/conftest.py
+++ tests/ui/conftest.py
@@ -1,24 +1,30 @@
 import pytest
+import allure
 
-from playwright.sync_api import sync_playwright
+from playwright.sync_api import sync_playwright, Browser, Page
 
 
 from pages.login_page import LoginPage
 from utils.helpers import CLICKUP_EMAIL, CLICKUP_PASSWORD
 
 @pytest.fixture(scope="session")
-def browser():
-    playwright = sync_playwright().start()  # Далее, используя объект playwright, можно запускать браузер и работать с ним
-    browser = playwright.chromium.launch(headless=False, slow_mo=1000)
-    yield browser
-    browser.close()
-    playwright.stop()
+def browser() -> Browser:
+    with allure.step("Запуск Playwright и инициализация браузера Chromium"):
+        playwright = sync_playwright().start()
+        browser = playwright.chromium.launch(headless=False, slow_mo=1000)
+        yield browser
+        with allure.step("Закрытие браузера и остановка Playwright"):
+            browser.close()
+            playwright.stop()
 
 
 @pytest.fixture(scope="session")
-def login(browser):
-    context = browser.new_context()
-    page = context.new_page()
-    LoginPage(page).login(CLICKUP_EMAIL, CLICKUP_PASSWORD)
-
+def login(browser: Browser) -> Page:
+    with allure.step("Создание контекста браузера и новой страницы"):
+        context = browser.new_context()
+        page = context.new_page()
+    with allure.step("Вход в ClickUp через UI"):
+        LoginPage(page).login(CLICKUP_EMAIL, CLICKUP_PASSWORD)
     yield page
+    with allure.step("Закрытие контекста после завершения сессии"):
+        context.close()
