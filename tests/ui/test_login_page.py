6ecefd734f435ab53d5828e1091c3b8f3fc9a36f
realandrey <andrewbarulin@gmail.com>
2025-05-19 22:32:27 +0300

    add allure steps

--- tests/ui/test_login_page.py
+++ tests/ui/test_login_page.py
@@ -1,3 +1,5 @@
+import allure
+
 from playwright.sync_api import expect
 
 from pages.login_page import LoginPage
@@ -5,19 +7,30 @@ from tests.ui.conftest import browser
 from utils.helpers import CLICKUP_EMAIL
 import pytest
 
+
+@allure.feature("Login functionality")
 class TestLoginPage:
 
+    @allure.description("Проверка успешного входа на страницу с валидными данными")
     def test_login_success(self, login):
-        login.wait_for_selector("text=Automate")  # дождаться элемента
-        assert "Automate" in login.content()
+        with allure.step("Ожидание появления текста 'Automate' на странице"):
+            login.wait_for_selector("text=Automate")  # дождаться элемента
+        with allure.step("Проверка, что на странице есть текст 'Automate'"):
+            assert "Automate" in login.content()
 
+    @allure.description("Проверка ошибки при вводе неправильного пароля")
     def test_login_negative(self, browser):
         page = browser.new_page()
         login_page = LoginPage(page)
-        login_page.login_with_invalid_password(CLICKUP_EMAIL, 'this_pass_is_wrong_123!')
 
-        error_locator = page.locator('[data-test="form__error"]')
-        expect(error_locator).to_be_visible(timeout=15000)
+        with allure.step(f"Попытка войти с неправильным паролем для {CLICKUP_EMAIL}"):
+            login_page.login_with_invalid_password(CLICKUP_EMAIL, 'this_pass_is_wrong_123!')
 
-        assert 'Incorrect password for this email.' in error_locator.inner_text()
-        assert not page.locator("text=Automate").is_visible(timeout=3000)
+        with allure.step("Проверка, что появилась ошибка 'Incorrect password for this email.'"):
+            error_locator = page.locator('[data-test="form__error"]')
+            expect(error_locator).to_be_visible(timeout=15000)
+
+            assert 'Incorrect password for this email.' in error_locator.inner_text()
+
+        with allure.step("Проверка, что текст 'Automate' на странице отсутствует"):
+            assert not page.locator("text=Automate").is_visible(timeout=3000)
